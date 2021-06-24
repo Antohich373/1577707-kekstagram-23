d37fa7b8fe7b269d3a526c7fa52bef1bc46f4281
Anton <gluskov352@gmail.com>
2021-06-24 07:58:42 +0300

    Задание 2 часть2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-let checingString = function(strings, maxLenght) {
+const checingString = function(strings, maxLenght) {
     if(strings.length <= maxLenght) {
         return true
     }
     return false
-}
-
-console.log(checingString(10, 100))
+};
+checingString(10, 100);
 
 const authorNumber = function(min, max){
     if(min >= max || max <= min || min < 0 || max < 0) {
-    throw(new Error('Ошибка - введите числа корректно'))
+    throw(new Error('Ошибка - введите числа корректно'));
     }
     return String('0' + (Math.floor(Math.random() * (max - min) + min)));
 };
-    
-console.log(authorNumber(11, 100))
+authorNumber(11, 100);
