cdd23ffd4c2aad627d2d1d5af271ac5f9e9dd744
Anton <gluskov352@gmail.com>
2021-06-27 09:37:02 +0300

    Задание 2 чать 3(тест)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-const checingString = function(strings, maxLenght) {
-    if(strings.length <= maxLenght) {
-        return true
-    }
-    return false
-};
-checingString(10, 100);
-
-const authorNumber = function(min, max){
-    if(min >= max || max <= min || min < 0 || max < 0) {
-    throw(new Error('Ошибка - введите числа корректно'));
-    }
-    return String('0' + (Math.floor(Math.random() * (max - min) + min)));
-};
-authorNumber(11, 100);
+import './generation.js'
